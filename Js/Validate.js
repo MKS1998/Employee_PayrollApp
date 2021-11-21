01619dee3e5286156df7cb17b618c4a6a7697fd0
MKS1998 <nksayyad2900@outlook.com>
2021-11-21 14:15:18 +0530

    Added : Ability to save the Employee Payroll Object to local storage

--- Js/Validate.js
+++ Js/Validate.js
@@ -2,86 +2,86 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const name = document.querySelector("#name");
     const textError = document.querySelector(".text-error");
     name.addEventListener("input", function () {
-        if (name.value.length == 0) {
-            textError.textContent = "";
-            return;
-        }
-        try {
-            new PersonInfo().name = name.value;
-            textError.textContent = "";
-        } catch (e) {
-            textError.textContent = e;
-        }
+      if (name.value.length == 0) {
+        textError.textContent = "";
+        return;
+      }
+      try {
+        new PersonInfo().name = name.value;
+        textError.textContent = "";
+      } catch (e) {
+        textError.textContent = e;
+      }
     });
-
+  
     const salary = document.querySelector("#salary");
     const output = document.querySelector(".salary-output");
     output.textContent = salary.value;
     salary.addEventListener("input", function () {
-        output.textContent = salary.value;
+      output.textContent = salary.value;
     });
-});
-
-const save = () => {
+  });
+  
+  const save = () => {
     try {
-        let employeePayrollData = createEmployeePayroll();
-        createAndUpdateStorage(employeePayrollData);
+      let employeePayrollData = createEmployeePayroll();
+      createAndUpdateStorage(employeePayrollData);
     } catch (e) {
-        return;
+      return;
     }
-};
-
-const createEmployeePayroll = () => {
+  };
+  
+  const createEmployeePayroll = () => {
     let employeePayrollData = new PersonInfo();
     try {
-        employeePayrollData.name = getInputValueById("#name");
+      employeePayrollData.name = getInputValueById("#name");
     } catch (e) {
-        setTextValue(".text-error", e);
-        throw e;
+      setTextValue(".text-error", e);
+      throw e;
     }
-
+  
     employeePayrollData.profilePic = getSelectedValues("[name=profile]").pop();
     employeePayrollData.gender = getSelectedValues("[name=gender]").pop();
     employeePayrollData.department = getSelectedValues("[name=department]");
     employeePayrollData.salary = getInputValueById("#salary");
     employeePayrollData.note = getInputValueById("#notes");
     let date =
-        getInputValueById("#day") +
-        " " +
-        getInputValueById("#month") +
-        " " +
-        getInputValueById("#year");
+      getInputValueById("#day") +
+      " " +
+      getInputValueById("#month") +
+      " " +
+      getInputValueById("#year");
     employeePayrollData.start_date = Date.parse(date);
     alert(employeePayrollData.toString());
     return employeePayrollData;
-};
-
-const getSelectedValues = (propertyValue) => {
+  };
+  
+  const getSelectedValues = (propertyValue) => {
     let allItems = document.querySelectorAll(propertyValue);
     let selItems = [];
     allItems.forEach((item) => {
-        if (item.checked) selItems.push(item.value);
+      if (item.checked) selItems.push(item.value);
     });
     return selItems;
-};
-
-const getInputValueById = (id) => {
+  };
+  
+  const getInputValueById = (id) => {
     let value = document.querySelector(id).value;
     return value;
-};
-
-function createAndUpdateStorage(employeePayrollData) {
+  };
+  
+  function createAndUpdateStorage(employeePayrollData) {
     let employeePayrollList = JSON.parse(
-        localStorage.getItem("EmployeePayrollList")
+      localStorage.getItem("EmployeePayrollList")
     );
     if (employeePayrollList != undefined) {
-        employeePayrollList.push(employeePayrollData);
+      employeePayrollList.push(employeePayrollData);
     } else {
-        employeePayrollList = [employeePayrollData];
+      employeePayrollList = [employeePayrollData];
     }
     alert(employeePayrollList.toString());
     localStorage.setItem(
-        "EmployeePayrollList",
-        JSON.stringify(employeePayrollList)
+      "EmployeePayrollList",
+      JSON.stringify(employeePayrollList)
     );
-}
+  }
